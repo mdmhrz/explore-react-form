396e898fff736c08b617831992c9687f05edf267
mdmhrz <razufreelence@gmail.com>
2025-05-30 18:22:03 +0600

    Form Locaton changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import ControlledField from './components/SimpleForm/ControlledField/ControlledField'
-import FormAction from './components/SimpleForm/FormAction/FormAction'
+import ControlledField from './components/ControlledField/ControlledField'
+import FormAction from './components/FormAction/FormAction'
 import UncontrolledField from './components/UncontrolledField/UncontrolledField'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 
@@ -13,8 +13,8 @@ function App() {
       <h1>Explore React Form</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <FormAction></FormAction> */}
-      {/* <ControlledField></ControlledField> */}
-      <UncontrolledField></UncontrolledField>
+      <ControlledField></ControlledField>
+      {/* <UncontrolledField></UncontrolledField> */}
     </>
   )
 }
